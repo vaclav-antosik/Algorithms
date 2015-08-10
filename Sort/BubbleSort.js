59377f0b20be30e726a3bbf51e1bae2e01a7c7ab
vaclav-antosik <vaclavantosik@hotmail.com>
2015-08-10 07:14:20 +0200

    BubbleSort optimalizations

--- Sort/BubbleSort.js
+++ Sort/BubbleSort.js
@@ -5,8 +5,8 @@ var guard = require("../guard");
 function sort(input) {
     guard.isArray(input);
     
-    for (var iCounter = 1; iCounter <= input.length - 1; iCounter++) {
-        for (var i = 0; i < input.length - 1; i++) {
+    for (var iCounter = 1, inputLength = input.length; iCounter <= input.length - 1; iCounter++) {
+        for (var i = 0, inputLength = input.length; i < input.length - iCounter; i++) {
             if (input[i] > input[i + 1]) {
                 var buff = input[i];
                 input[i] = input[i + 1];
--- Test/Sort/BubbleSortTest.js
+++ Test/Sort/BubbleSortTest.js
@@ -14,4 +14,8 @@ describe("BubbleSort", function () {
     it("should be [1,2,3] when input is [3,2,1]", function () {
         bubbleSort.sort([3, 2, 1]).should.eql([1, 2, 3]);
     });
+
+    it("should be [-3,-2,-1,0,1,2] when input is [2,1,0,-2,-1,-3]", function () {
+        bubbleSort.sort([2, 1, 0, -2, -1, -3]).should.eql([-3, -2, -1, 0, 1, 2])
+    });
 })
