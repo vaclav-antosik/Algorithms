8261958c3d1d5a0a719b3d224d0becbf0ed0ff5e
vaclav-antosik <vaclavantosik@hotmail.com>
2015-08-18 07:28:48 +0200

    Insertion sort

---
+++ Sort/InsertionSort.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+var guard = require("../Guard");
+var helpers = require("../Helpers")
+
+function sort(input) {
+    guard.isArray(input);
+    
+    var startTime = process.hrtime();
+
+    var inputLength = input.length;
+    
+    for (var i = 1; i < inputLength; i++) {
+        
+        var j = i - 1;
+        var tmp = input[i];
+        
+        while (tmp < input[j] && j >= 0) {
+            input[j + 1] = input[j];
+            j--;
+        }
+        input[j+1] = tmp;
+    }
+    
+    helpers.writeTime(startTime);
+    
+    return input;
+}
+
+module.exports.sort = sort;
--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 ﻿'use strict';
 
-var generator = require("./Test/GenerateSortTestFile");
+//var generator = require("./Test/GenerateSortTestFile");
 
-generator.generateSortTestFile("./Test/!TestData/big.json", "sort.json");
+//generator.generateSortTestFile("./Test/!TestData/big.json", "sort.json");
+
+var insertionSort = require("./Sort/InsertionSort");
+
+var result = insertionSort.sort([3,2,1]);
 
 console.log("done");
